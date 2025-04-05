aa7459eb21d1690b82060ae7c625b1b055c441a8
Perry280 <mason.alessio.am@gmail.com>
2025-04-06 00:00:41 +0200

    Update clangd.lua

--- neovim/Linux/nvim/lsp/clangd.lua
+++ neovim/Linux/nvim/lsp/clangd.lua
@@ -1,4 +1,13 @@
 return{
     cmd = { "clangd" },
-    filetypes = { "c", 'cpp' }
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    capabilities = {
+      offsetEncoding = { "utf-8", "utf-16" },
+      textDocument = {
+        completion = {
+          editsNearCursor = true
+        }
+      }
+    },
+    single_file_support = true
 }
