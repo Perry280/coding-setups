2b2ce6afa87e8527904ec17d6a350ae1a4943044
Perry280 <mason.alessio.am@gmail.com>
2025-04-09 17:30:47 +0200

    Add files via upload

--- neovim/Linux/nvim/lsp/luals.lua
+++ neovim/Linux/nvim/lsp/luals.lua
@@ -1,23 +1,23 @@
-return{
+return {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-                path = { "lua/?.lua", "lua/?/init.lua" }
-            },
-            workspace = {
-                library = vim.tbl_extend(
-                    "keep",
-                    { vim.env.VIMRUNTIME, "${3rd}/luv/library" },
-                    vim.api.nvim_get_runtime_file("", true)
-                ),
-                checkThirdParty = false,
-            },
-            diagnostics = { globals = { "vim" } },
-            telemetry = { enable = false },
-        },
-    },
     root_markers = { ".luarc.json", ".luarc.jsonc" },
+    -- settings = {
+    --     Lua = {
+    --         runtime = {
+    --             version = "LuaJIT",
+    --             path = { "lua/?.lua", "lua/?/init.lua", "lua/lsp/lsp_features.lua" }
+    --         },
+    --         workspace = {
+    --             library = vim.tbl_extend(
+    --                 "keep",
+    --                 { vim.env.VIMRUNTIME, "${3rd}/luv/library" },
+    --                 vim.api.nvim_get_runtime_file("", true)
+    --             ),
+    --             checkThirdParty = false,
+    --         },
+    --         diagnostics = { globals = { "vim" } },
+    --         telemetry = { enable = false },
+    --     },
+    -- },
 }
