0c49377eef24853b7e9bfd7b865d33d84448223b
Perry280 <174331803+Perry280@users.noreply.github.com>
2025-05-06 15:02:41 +0200

    Added mini.surround

--- neovim/nvim/lua/plugins/mini.lua
+++ neovim/nvim/lua/plugins/mini.lua
@@ -1,22 +1,31 @@
 return {
-    'echasnovski/mini.pairs',
-    version = '*',
-    config = function()
-        require('mini.pairs').setup({
-            modes = { insert = true, command = true, terminal = false },
-            -- skip autopair when next character is one of these
-            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-            -- skip autopair when the cursor is inside these treesitter nodes
-            skip_ts = { "string" },
-            -- skip autopair when next character is closing pair
-            -- and there are more closing pairs than opening pairs
-            skip_unbalanced = true,
-            -- better deal with markdown code blocks
-            markdown = true,
-            -- mappings = {
-            --     ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
-            --     ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
-            -- },
-        })
-    end
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        config = function()
+            require('mini.pairs').setup({
+                modes = { insert = true, command = true, terminal = false },
+                -- skip autopair when next character is one of these
+                skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+                -- skip autopair when the cursor is inside these treesitter nodes
+                skip_ts = { "string" },
+                -- skip autopair when next character is closing pair
+                -- and there are more closing pairs than opening pairs
+                skip_unbalanced = true,
+                -- better deal with markdown code blocks
+                markdown = true,
+                -- mappings = {
+                --     ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+                --     ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+                -- },
+            })
+        end
+    },
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        config = function()
+            require('mini.surround').setup()
+        end
+    },
 }
