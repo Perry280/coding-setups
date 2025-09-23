ed4f8069b3b9fcfa8c7aee6d291e0493d24a8210
Perry280 <174331803+Perry280@users.noreply.github.com>
2025-09-23 23:36:31 +0200

    colorscheme.lua

---
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     lazy = false,
+    --     priority = 1000,
+    -- },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    -- },
+    -- {
+    --     "EdenEast/nightfox.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    -- },
+    -- {
+    --     "navarasu/onedark.nvim",
+    --     priority = 1000,
+    --     config = function()
+    --         require('onedark').setup {
+    --             style = 'warmer'
+    --         }
+    --     end
+    -- },
+    -- {
+    --     "sainnhe/sonokai",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd("set termguicolors")
+    --
+    --         vim.g.sonokai_style = 'andromeda'
+    --         vim.g.sonokai_better_performance = 1
+    --     end
+    -- },
+}
