42ecc85bdcc13dbd30104b21166d64f2a6f79dd3
Perry280 <174331803+Perry280@users.noreply.github.com>
2025-09-28 17:05:34 +0200

    windows.lua

--- neovim/nvim/lua/setup/windows.lua
+++ neovim/nvim/lua/setup/windows.lua
@@ -1,5 +1,3 @@
--- Add this file in ~/.config/nvim/lua/setup/ when using neovim on Windows
-
 vim.opt.shell = "pwsh.exe"
 vim.opt.shellcmdflag =
 "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.Formatting.Error = '';$PSStyle.Formatting.ErrorAccent = '';$PSStyle.Formatting.Warning = '';$PSStyle.OutputRendering = 'PlainText';"
